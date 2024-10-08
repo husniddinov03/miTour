58b78be16c968c66defae064d2b8eaa5b9779320
Otabek <xusniddinovotabek00@gmail.com>
2024-10-08 10:50:22 +0500

    full version -v2

--- src/Routes/TourExcursions/Pages/Attractions/Attractions.jsx
+++ src/Routes/TourExcursions/Pages/Attractions/Attractions.jsx
@@ -4,48 +4,48 @@ import "./Style.scss"
 import advImg from './advImg'
 import { useTranslation } from 'react-i18next'
 
+
 const Attractions = () => {
-  const { t } = useTranslation("globbal")
+const {t}=useTranslation("globbal")
 
-  const dataPage = [
-    {
-      id: 1,
-      text: "itemNavTour.selfDrive",
-      to: "/selfDrive",
-      active: false
-    },
-    {
-      id: 2,
-      text: "itemNavTour.advantures",
-      to: "/tourExcursions",
-      active: false
-    },
-    {
-      id: 3,
-      text: "itemNavTour.attractions",
-      to: "/attractions",
-      active: true
-    },
-    {
-      id: 4,
-      text: "itemNavTour.sightseeing",
-      to: "/sightseeing",
-      active: false
-    },
-
-  ]
+const dataPage = [
+  {
+    id: 1,
+    text: "itemNavTour.advantures",
+    to: "/tourExcursions",
+    active: false
+  },
+  {
+    id: 2,
+    text: "itemNavTour.attractions",
+    to: "/attractions",
+    active: true
+  },
+  {
+    id: 3,
+    text: "itemNavTour.sightseeing",
+    to: "/sightseeing",
+    active: false
+  },
+  {
+    id: 4,
+    text: "itemNavTour.selfDrive",
+    to: "/selfDrive",
+    active: false
+  },
+]
 
   return (
-    <div className='attractions resp-box2'>
+    <div className='attractions containerElement'>
       <PageNavbar dataPage={dataPage} />
 
-      <h1 style={{ fontWeight: "660" }} className=''>{t("attractions.title")}</h1><br />
-      <p className='default-desc-text'>{t("attractions.description")}</p>
-      <div className="attractions-carousel">
-        <CarouselBox data={advImg} slidesPerView={3} width="1340px"/>
+      <div className="attractions">
+        <h1 className='default-title-text'>{t("attractions.title")}</h1><br />
+        <p className='default-desc-text'>{t("attractions.description")}</p>
+        <div className="attractions-carousel">
+          <CarouselBox data={advImg} slidesPerView={3} />
+        </div>
       </div>
-      <br /><br />
-      <br />
 
     </div>
   )
